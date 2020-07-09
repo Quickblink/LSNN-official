45485a698d8847ce845f7868f0445b88d2c0f25e
Eric <eric.koepke@tum.de>
2020-07-09 23:35:02 +0200

    transfer to vm 3

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 docker build -t imlsnn . \
-&& docker run \
+&& nvidia-docker run \
 -v /home/eric/LSNN-official:/home/developer \
 --name lsnn_co \
 --net host \
 --ipc host \
-imlsnn
+--rm \
+imlsnn \
 python3 bin/my_run.py
